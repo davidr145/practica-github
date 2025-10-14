bbdb602c477e13c7eea0b4757cb9c28e3aaa9123
davidr145 <david.rio@campus.xaloc.org>
2025-10-14 10:08:15 +0200

    Add files via upload

---
+++ ej19.py
@@ -0,0 +1,6 @@
+numero1 = float(input("introduce el primer valor: "))
+numero2 =float(input("introduce el segundo valor:"))
+if numero1 > numero2:
+    print(f"el {numero1} es mayor que el {numero2}")
+else:
+    print(f"el  {numero2} es mayor que el 4{numero1}")
